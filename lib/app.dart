e8cc8fb7899217612851b04bb0b65687b4b183ba
Astika <grgastika@gmail.com>
2023-05-21 10:03:51 +0545

    app

---
+++ lib/app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:listview_batch30/view/card_view.dart';
+import 'package:listview_batch30/view/edit_student_view.dart';
+import 'package:listview_batch30/view/output_view.dart';
+import 'package:listview_batch30/view/student_view.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Student Details',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const StudentView(),
+        '/outputRoute': (context) => const OutputView(),
+        '/editStudentRoute': (context) => const EditStudentView(),
+        '/student': (context) => const StudentView(),
+        '/cardRoute': (context) => const CardView(),
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:listview_batch30/app.dart';
 import 'package:listview_batch30/view/card_view.dart';
 import 'package:listview_batch30/view/edit_student_view.dart';
 import 'package:listview_batch30/view/output_view.dart';
 import 'package:listview_batch30/view/student_view.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Student Details',
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const StudentView(),
-        '/outputRoute': (context) => const OutputView(),
-        '/editStudentRoute': (context) => const EditStudentView(),
-        '/student': (context) => const StudentView(),
-        '/cardRoute': (context) => const CardView(),
-      },
-    ),
-  );
+  runApp(const App());
 }
